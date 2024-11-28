3f10b278bacb5012fe06bd49af1ef0e1fe8c84bf
Abdullah mohamed Elkohly <116310489+abdulaah345@users.noreply.github.com>
2024-11-28 16:13:45 +0200

    v90

--- Booking/src/pages/home/Home.jsx
+++ Booking/src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
 import Featured from '../../component/featured/Featured';
+import Featuredprop from '../../component/featuredproperties/Featuredprop';
 import Header from '../../component/header/Header';
+import Maillist from '../../component/maillist/Maillist';
 import Navbar from '../../component/navbar/navbar';
 import Properiteslist from '../../component/properiteslist/properiteslist';
 import './home.css';
@@ -12,9 +14,14 @@ const Home = () => {
    <div className="homecontainer">
     <Featured/>
     <h1 className="hometitle">
+    Browse by property type
+        </h1>
+    <Properiteslist/>
+    <h1 className="hometitle">
     Homes guests love
     </h1>
-    <Properiteslist/>
+    <Featuredprop/>
+    <Maillist/>
    </div>
     </div>
   );
